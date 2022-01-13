d759854da1f9047513ee8e36008f97e389c12d4f
keshavpwc <keshav.k.verma@pwc.com>
2022-01-13 13:35:16 +0530

    null pointer exception resolve

--- rainmaker-pgr/src/main/java/org/egov/pgr/repository/rowmapper/SwatchBharatRowMapper.java
+++ rainmaker-pgr/src/main/java/org/egov/pgr/repository/rowmapper/SwatchBharatRowMapper.java
@@ -43,8 +43,8 @@ public class SwatchBharatRowMapper implements ResultSetExtractor<List<SwatchBhar
 				currentSwatchBharat = SwatchBharat.builder().useruuid(rs.getObject("useruuid").toString())
 						.fileid(rs.getObject("fileid").toString()).uuid(rs.getObject("uuid").toString())
 						.isvalidimage(Boolean.parseBoolean(rs.getObject("isvalidimage").toString()))
-						.username(rs.getObject("username").toString())
-						.workbookid(Long.parseLong(rs.getObject("workbookid").toString()))
+						.username(rs.getObject("username") != null ? rs.getObject("username").toString() : "")
+						.workbookid(Long.parseLong(rs.getObject("workbookid") != null ? rs.getObject("workbookid").toString() : ""))
 						.auditDetails(auditDetails).build();
 				swatchBharatMap.put(id, currentSwatchBharat);
 			}
