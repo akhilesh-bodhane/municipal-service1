5272774bb4dab99dda3c9cc4d6489d5f9754ee18
prakashhadgal <hadgalprakash92@gmail.com>
2022-03-31 15:42:04 +0530

    Get Application issue

--- hc-services/src/main/java/org/egov/hc/repository/ServiceRepository.java
+++ hc-services/src/main/java/org/egov/hc/repository/ServiceRepository.java
@@ -93,7 +93,7 @@ public class ServiceRepository {
 
 				} else {
 					actualResult = (JSONArray) jdbcTemplate.query(queryBuilder.SELECT_SERVICE_DETAIL,
-							new Object[] { requestData.getService_request_id() }, rowMapper);
+							new Object[] { requestData.getService_request_id(), requestData.getIsUT() }, rowMapper);
 					log.info("Get service getails ");
 				}
 				JSONArray jsonArray = new JSONArray();
