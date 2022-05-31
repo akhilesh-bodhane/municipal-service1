22baecd44a658facc4a28f3d650ec7d82503a139
Digambar2021 <digambar.rajgire@transerve.com>
2022-05-31 10:24:03 +0530

    Update Api Give Property

--- ws-services/src/main/java/org/egov/waterconnection/model/WaterConnection.java
+++ ws-services/src/main/java/org/egov/waterconnection/model/WaterConnection.java
@@ -5,16 +5,31 @@ import java.util.Objects;
 
 import javax.validation.constraints.Size;
 
+import org.egov.waterconnection.model.Property;
+
 import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * WaterConnection
  */
 @Validated
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-03-11T12:10:14.583+05:30[Asia/Kolkata]")
 public class WaterConnection extends Connection {
 
@@ -22,6 +37,9 @@ public class WaterConnection extends Connection {
 
 	@JsonProperty("waterSource")
 	private String waterSource = null;
+	
+	@JsonProperty("property")
+	private Property property;
 
 	@JsonProperty("meterId")
 	private String meterId = null;
