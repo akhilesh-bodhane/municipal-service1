2f2d953900c6e6fd03ca2dec6edb322750cef4d9
prakashhadgal <hadgalprakash92@gmail.com>
2022-11-15 16:35:08 +0530

    New changes for pgr

--- rainmaker-pgr/src/main/java/org/egov/pgr/model/GrievenceReport.java
+++ rainmaker-pgr/src/main/java/org/egov/pgr/model/GrievenceReport.java
@@ -1,5 +1,6 @@
 package org.egov.pgr.model;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -38,7 +39,7 @@ public class GrievenceReport {
 	public String slaAchievement;
 
 	@JsonProperty("completionRate")
-	public Double completionRate;
+	public BigDecimal completionRate;
 
 	@JsonProperty("uniqueCitizens")
 	public Integer uniqueCitizens;
