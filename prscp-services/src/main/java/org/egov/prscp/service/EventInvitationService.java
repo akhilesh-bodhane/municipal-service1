59e85febaab8c269574846753cb95d1c75edac05
keshavpwc <keshav.k.verma@pwc.com>
2023-08-03 09:55:41 +0530

    file upload issue xls prscp

--- prscp-services/src/main/java/org/egov/prscp/service/EventInvitationService.java
+++ prscp-services/src/main/java/org/egov/prscp/service/EventInvitationService.java
@@ -108,22 +108,23 @@ public class EventInvitationService {
 //			int lastIndexOf1 = fileUrls.indexOf(".xls")-1;
 			
 //			String filename=fileUrls.substring(lastIndexOf, lastIndexOf1).replaceAll(" ", "%20");
-			String filename=fileUrls.replaceAll(" ", "%20");
-			log.info("filename : " + filename);
+//			String filename=fileUrls.replaceAll(" ", "%20");
+//			log.info("filename : " + filename);
 			StringBuilder string = new StringBuilder(fileUrls);			
 //			string.replace(lastIndexOf,lastIndexOf1 , filename);
-			UrlResource fileResource = new UrlResource(string.toString());
-			/*
+//			UrlResource fileResource = new UrlResource(string.toString());
+			
 			CloseableHttpClient client = HttpClientBuilder.create().build();
 			HttpGet request = new HttpGet(fileUrls.replaceAll(" ", "%20"));
 			request.addHeader("accept", "application/vnd.ms-excel");
 			HttpResponse response = client.execute(request);
 			HttpEntity entity = response.getEntity();
 			int responseCode = response.getStatusLine().getStatusCode();
-			InputStream inputStream = entity.getContent();*/
+			InputStream inputStream = entity.getContent();
 			
 			List<InviteGuest> userList = new ArrayList<>();
-			HSSFWorkbook workbook = new HSSFWorkbook(fileResource.getInputStream());
+//			HSSFWorkbook workbook = new HSSFWorkbook(fileResource.getInputStream());
+			HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
 			HSSFSheet worksheet = workbook.getSheetAt(0);
 			Iterator<Row> rowIterator = worksheet.iterator();
 			rowIterator.next(); // skip the header row
