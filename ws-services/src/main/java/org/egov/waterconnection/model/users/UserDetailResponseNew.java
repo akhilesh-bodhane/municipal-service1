e3b139b4e9ab69cc30823134af36e53db4bd4acf
keshavpwc <keshav.k.verma@pwc.com>
2023-08-25 09:43:07 +0530

    log

--- ws-services/src/main/java/org/egov/waterconnection/model/users/UserDetailResponseNew.java
+++ ws-services/src/main/java/org/egov/waterconnection/model/users/UserDetailResponseNew.java
@@ -12,10 +12,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
 public class UserDetailResponseNew {
     @JsonProperty("responseInfo")
     ResponseInfo responseInfo;
