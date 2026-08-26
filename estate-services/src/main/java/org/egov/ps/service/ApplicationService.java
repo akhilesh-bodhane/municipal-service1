1216d94541bbf1f94b3e682e342f0f13e0b541f1
akhilesh-bodhane <akhilesh.bodhane@pwc.com>
2026-08-26 19:41:46 +0530

    sampark employee seach property access

--- estate-services/src/main/java/org/egov/ps/service/ApplicationService.java
+++ estate-services/src/main/java/org/egov/ps/service/ApplicationService.java
@@ -103,10 +103,7 @@ public class ApplicationService {
 						if (rolecode.startsWith("ES_BB")) {
 							employeeBranches.add(PSConstants.APPLICATION_BUILDING_BRANCH);
 						}
-						if (rolecode.startsWith("ES_MM")) {
-							employeeBranches.add(PSConstants.APPLICATION_MANI_MAJRA);
-						}
-						if (rolecode.startsWith("SAMPARK")) {
+						if (rolecode.startsWith("ES_MM") || rolecode.startsWith("SAMPARK")) {
 							employeeBranches.add(PSConstants.APPLICATION_MANI_MAJRA);
 						}
 						if (rolecode.equalsIgnoreCase("ES_ADDITIONAL_COMMISSIONER")) {
